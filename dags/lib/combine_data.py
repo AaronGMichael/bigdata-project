fa4d7eedae5d20d5fe68c20c8b3423a78996265c
Aaron M <aaron.g.michael@gmail.com>
2024-06-15 16:28:10 +0200

    v1 for combine

--- dags/lib/combine_data.py
+++ dags/lib/combine_data.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 from pyspark.sql import SQLContext
-import convert_data
+
 from pyspark import SparkContext
 from pyspark.sql.functions import lit
 
@@ -14,17 +14,26 @@ os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 cwd = os.getcwd()  # Get the current working directory (cwd)
 cwd = cwd.replace("\\", "/")
 cwd = cwd + "/../datalake2/"
-DATALAKE_ROOT_FOLDER = cwd
+DATALAKE_ROOT_FOLDER = "../../datalake2/"
 
 
-def combine_data(files):
+def list_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            r.append(os.path.join(root, name).replace('\\', '/'))
+    return r
+
+def combine_data():
     sc = SparkContext(appName="CombineData", master="local[*]")
     sqlContext = SQLContext(sc)
     curr_pits_path = None
     df_pits = None
     allData = None
-    out = "../datalake/"
+    out = "datalake2/"
+    files = list_files(DATALAKE_ROOT_FOLDER + "formatted/lapData/")
     for f in files:
+        print(f)
         OUTPUT_FOLDER = f.replace("formatted/lapData", "usage").replace(f.split('/')[-1], "")
         year = re.search(r"(\d{4}\b)", f).group(0)
         driverName = OUTPUT_FOLDER.split("/")[-2]
@@ -49,4 +58,5 @@ def combine_data(files):
                   .withColumn("circuit", lit(circuitName)))
         stints.write.save(DATALAKE_ROOT_FOLDER + "usage/" + "stints", mode="append")
 
-# combine_data(convert_data.list_files(DATALAKE_ROOT_FOLDER + "formatted/lapData/"))
+combine_data()
+
